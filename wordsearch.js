ff87dc307f2659eb9764d99ef1495474bd630938
MeganTherion <meganhutt@hotmail.com>
2022-04-22 21:42:22 +0000

    complete wordsearch challenge

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,41 @@
+
+
+
+
+
+
+//const transpose = require('/..transpose')
+
+const transpose = function (matrix) {
+    let result = [];
+    const rows = matrix[0].length;
+    const cols = matrix.length;
+    for (let i = 0; i < rows; i++) {
+      const newRow = [];
+      for (let j = 0; j < cols; j++) {
+        newRow.push(matrix[j][i]);
+      }
+      result.push(newRow);
+    }
+    return result;
+  };
+
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    const verticalJoin = transpose(letters).map(ls => ls.join(''))
+    const newArray = horizontalJoin.concat(verticalJoin);
+    console.log('horizontaljoin', horizontalJoin);
+    console.log('verticalJoin', verticalJoin);
+    console.log('newArray', newArray)
+    // for (x of verticalJoin) {
+    //     if (x.includes(word)) return true;
+    // }
+    for (l of newArray) {
+      if (l.includes(word)) return true;
+     } if (!l.includes(word)) return false;
     }
-}
+
+
+
 
 module.exports = wordSearch
